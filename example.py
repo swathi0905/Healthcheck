23769685ebed27beefd57930f2c0754e034d6d7f
swathi0905 <98885759+swathi0905@users.noreply.github.com>
2022-11-03 18:17:43 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,14 @@
-def git_operation():
-	print("I am adding example.py file to the remote repository.")
-git_operation()
+import shutil
+import psutil
+
+def chech_disk_usage(disk):
+	du = shutil.disk_usage(disk)
+	free = du.free / du.total * 100
+	return free > 20
+def check_cpu_usage():
+	usage = psutil.cpu_percent(1)
+	return usage < 75
+if not check_disk_usage("/") or not check_cpu_usage():
+	print("ERROR")
+else:
+	print("Everything is okay")
